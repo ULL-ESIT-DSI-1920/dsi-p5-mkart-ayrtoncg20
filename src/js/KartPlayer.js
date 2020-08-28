f5c67e598616ee450678631b9f937d604177f717
ayrtoncg20 <alu0100839739@ull.edu.es>
2020-08-28 16:58:04 +0100

    Creando los comentarios en el codigo

--- src/js/KartPlayer.js
+++ src/js/KartPlayer.js
@@ -1,13 +1,16 @@
-export class KartPlayer extends HTMLElement {
-  constructor(name, config) {
-    super();
+export class KartPlayer extends HTMLElement { //Hacemos uso de web component para definir la clase Kartplayer
+  constructor(name, config) { //creamos un constructor con el nombre y la configuracion del kart
+    super(); //La palabra clave super es usada para acceder y llamar funciones del padre de un objeto
     this.attachShadow({ mode: 'open' });
-    this.name = name;
-    this.y = config.y;
-    this.image = config.image;
-    this.x = 0;
+    this.name = name; //Este nombre es el nombre que se le pasa por parametro
+    this.y = config.y; //Esta y es la y que se le pasa por parametro
+    this.image = config.image; //Esta imagen es la imagen que se le pasa por parametro
+    this.x = 0; //Declaramos una variable x ya que el kart tiene variable x e y para saber en que posicion esta exactamente
 
   }
+
+  //Dentro de styles igualamos left a la variable x que habíamos creado para saber donde esta el kart
+  //Tambien creamos las clases winner y lost para saber si un kart ha ganado o ha perdido
 
   get styles() {
     return `
@@ -34,6 +37,7 @@ export class KartPlayer extends HTMLElement {
      `;
   }
 
+  //La funcion render mostrar los kart por pantalla haciendo uso de styles, le ponemos una imagen que sea la imagen pasada por parametro
   render() {
 
     this.shadowRoot.innerHTML = `
@@ -42,18 +46,19 @@ export class KartPlayer extends HTMLElement {
           <img src="${this.image}" alt="${this.name}">
         `;
   }
+
+  //Tenemos una funcion para añadir a la carretera
   addToRoad(road) {
-    road.appendChild(this);
+    road.appendChild(this); //nuevo nodo hijo
     this.render()
 
 
   }
-  inc() {
-    console.log(this.imagen_item)
-    this.x += Math.random() * (5 - 0) + 0;;
-    this.render()
+  inc() { //funcion para incrementar la posicion x de los karts
+    this.x += Math.random() * (5 - 0) + 0; //le añadimos un valor aleatorio entre 0 y 5 a la posicion x del kart
+    this.render() //llamamos a la funcion render para pintar los kart con las nuevas posiciones
   }
-  isWinner() {
+  isWinner() { //La funcion is winner comprueba si un kart en la posicion x ha llegado a 930 y lo da por ganador
     if (this.x >= 930) {
       return true;
     }
@@ -61,21 +66,21 @@ export class KartPlayer extends HTMLElement {
       return false;
     }
   }
-  win() {
+  win() { //Se llama a la funcion win por lo que esto le da unos estilos winner creados antes y volvemos a mostrar todo 
     this.shadowRoot.innerHTML = `
           <style>${this.styles}</style>
           <item-kart></item-kart>
           <img class="winner" src="${this.image}">
         `;
   }
-  lose() {
+  lose() { //Se llama a la funcion lose por lo que esto le da unos estilos lose creados antes y volvemos a mostrar todo
     this.shadowRoot.innerHTML = `
           <style>${this.styles}</style>
           <item-kart></item-kart>
           <img class="lose" src="${this.image}">
         `;
   }
-  restart() {
+  restart() { //para restablecer la carrera solo ponemos la x de cada kart a 0 y llamamos a render
     this.x = 0;
     this.render()
   }
--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,45 @@
-import players from "../assets/kart-*.png"
-import { KartPlayer } from "./KartPlayer.js";
+import players from "../assets/kart-*.png" //importamos los kart como players
+import { KartPlayer } from "./KartPlayer.js"; //importamos la clase Kartplayer
 
-const startButton = document.querySelector(".start");
-const restartButton = document.querySelector(".restart");
-const road = document.querySelector(".road");
-const karts = [];
+const startButton = document.querySelector(".start"); //creamos una constante para guardar el boton start
+const restartButton = document.querySelector(".restart"); //Creamos una constante para guardar el boton restart 
+const road = document.querySelector(".road"); //Creamos una constante para la carretera 
+const karts = []; //array para los kart
 let timer = null;
 
-for (const [name, image] of Object.entries(players)) {
+for (const [name, image] of Object.entries(players)) { //recorremos el objeto player que tiene el nombre del kart
     const config = {
         image,
-        y: karts.length * 64
+        y: karts.length * 64 //posicion y de cada kart
     };
-    const kart = new KartPlayer(name, config);
-    kart.addToRoad(road);
-    karts.push(kart);
+    const kart = new KartPlayer(name, config); //Creamos un nuevo kart con el nombre y un array de configuracion donde esta la imagen y la posicion y
+    kart.addToRoad(road); //añadimos el kart a la carretera
+    karts.push(kart); //Metemoe el kart en el array de kart
 }
 
-const startRace = () => {
+const startRace = () => { //Funcion para comenzar la carrera
     timer = setInterval(() => startIterarion(), 1000 / 60);
     startButton.disabled = true;
     restartButton.disabled = true;
 };
 
 
-const endRace = () => {
+const endRace = () => { //funcion para acabar la carrera
     clearInterval(timer);
     karts.forEach(kart => (kart.isWinner() ? kart.win() : kart.lose()));
     restartButton.disabled = false;
 };
 
-const restartRace = () => {
+const restartRace = () => { //funcion para restablecer la carrera
     karts.forEach(kart => kart.restart());
     startButton.disabled = false;
 };
 
-const startIterarion = () => {
+const startIterarion = () => { //funcion para avanzar los kart en cada iteracion
 
     karts.forEach(kart => kart.inc());
     if (karts.some(kart => kart.isWinner())) endRace();
 };
 
-startButton.onclick = () => startRace();
-restartButton.onclick = () => restartRace();
+startButton.onclick = () => startRace(); //si le damos click a start llamamos a la funcion que empieza la carrera
+restartButton.onclick = () => restartRace(); //si le damos click a restart llamamos a la funcion para restablecer la carrera. 
